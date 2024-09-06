76c13c80e2f222c113c4feb9f0c47c5bc2ddb293
paucabral <josepaucabral.gmail.com>
2024-09-06 23:24:21 +0800

    Add example for 2-tier architecture sampler

--- main.tf
+++ main.tf
@@ -1,9 +1,28 @@
-resource "virtualbox_vm" "node" {
+resource "virtualbox_vm" "db_node" {
   count     = 1
-  name      = var.vm_count == 1 ? var.vm_name : format("${var.vm_name}-%02d", count.index + 1)
+  name      = "db"
   image     = var.image
   cpus      = 2
-  memory    = "512 MB"
+  memory    = "1024 MB"
+  user_data = var.user_data
+
+  network_adapter {
+    type           = var.network_adapter_type
+    host_interface = var.network_host_interface
+  }
+
+  network_adapter {
+    type           = "nat"
+    host_interface = "NatNetwork"
+  }
+}
+
+resource "virtualbox_vm" "app_node" {
+  count     = 1
+  name      = "app"
+  image     = var.image
+  cpus      = 2
+  memory    = "1024 MB"
   user_data = var.user_data
 
   network_adapter {
--- outputs.tf
+++ outputs.tf
@@ -7,13 +7,22 @@
 # }
 
 locals {
-  instance_names = virtualbox_vm.node.*.name
-  ip_addresses = virtualbox_vm.node.*.network_adapter.0.ipv4_address
+  db_instance_names = virtualbox_vm.db_node.*.name
+  db_ip_addresses = virtualbox_vm.db_node.*.network_adapter.0.ipv4_address
+  app_instance_names = virtualbox_vm.app_node.*.name
+  app_ip_addresses = virtualbox_vm.app_node.*.network_adapter.0.ipv4_address
 }
 
-output "instance_ip" {
+output "db_instance_ip" {
   value = [
-    for idx, instance in local.instance_names :
-    "${instance}: ${local.ip_addresses[idx]}"
+    for idx, instance in local.db_instance_names :
+    "${instance}: ${local.db_ip_addresses[idx]}"
+  ]
+}
+
+output "app_instance_ip" {
+  value = [
+    for idx, instance in local.app_instance_names :
+    "${instance}: ${local.app_ip_addresses[idx]}"
   ]
 }
